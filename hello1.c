b6ff1c22db8de184a6cdb04a2bd39b6d4461b92c
nuly17 <nuly17@gmail.com>
2018-10-13 00:04:11 +0900

    Update hello1.c

--- hello1.c
+++ hello1.c
@@ -3,7 +3,11 @@
 int main()
 {
   int i = 0 ;
+  int j = 0;
+  
   for( i = 0; i < 11; i++ )
-    printf("hello world\n");
+      for( j = 0; j < i; j++ )
+        printf("hello world\n");
+  
   return 1;
 }
